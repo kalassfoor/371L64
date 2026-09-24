d94c94c6cbfd8f0febb7225097c1cb44f3024e16
kalassfoor <49289310+kalassfoor@users.noreply.github.com>
2026-09-24 22:01:01 +0300

    Add extra print statement in hello.java
    Added an additional print statement for 'khaled'.

--- src/hello.java
+++ src/hello.java
@@ -8,6 +8,7 @@ System.out.print(" hello, khaled,");
 System.out.print(" hello, khaled1,");
 System.out.print(" hello, khaled1,");
 System.out.print("khaled");
+		System.out.print("khaled");
 	}
 
 }
